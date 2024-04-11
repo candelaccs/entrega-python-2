a4e2504669fd9767547871d4c3b685e6039a2c7e
Candela Caceres <cande86@outlook.com>
2024-04-10 23:02:18 -0300

    creo def maximo goleador + quito def main para no tener problemas de indentacion

--- hockey/__init__.py
+++ hockey/__init__.py
@@ -2,6 +2,12 @@
 
 def structure (names, goals, goals_avoided, assists):
 
-   return list(zip(names,goals,goals_avoided,assists))
+   return list(zip(names,goals,goals_avoided,assists)) #me arma la lista con las estructuras que ya tenia 
 
 
+
+def max_player (players):
+
+   max_p = max(players, key=lambda x: x[1])
+
+   return max_p[0], max_p[1] #retorna nombre y cantidad de goles

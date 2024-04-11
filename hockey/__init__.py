a5ec15de3bfcc30efe68de67ab99205a0286799e
Candela Caceres <cande86@outlook.com>
2024-04-11 00:05:07 -0300

    añado def most influent player + pongo prints

--- hockey/__init__.py
+++ hockey/__init__.py
@@ -11,3 +11,9 @@ def max_player (players):
    max_p = max(players, key=lambda x: x[1])
 
    return max_p[0], max_p[1] #retorna nombre y cantidad de goles
+
+def most_inf (players):
+   
+   inf_player =  max(list, key= lambda x: x[1]*1.5+ x[2]*1.25+ x[3])
+
+   return (inf_player)
